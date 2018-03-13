3868dbace5ebdc9c96ccbfc2c7f4c826e230cb02
K4YT3X <k4t@protonmail.com>
2018-03-13 17:53:00 -0400

    v1.2 enhanced argument processing and etc.

--- shadowagent.py
+++ shadowagent.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
    ____   __            __                ___                     __
@@ -11,18 +11,19 @@
 Name: ShadowAgent
 Author: K4YT3X
 Description: ShadowAgent stands for "Silkroad Controller". It is essentially a
-shadowsocks client for now. However, in the future, it will become a client for
-shadowgate
+shadowsocks client for now. However, in the future, it is expected to become a
+client for shadowgate
 
 Date of Creation: July 27,2017
-Last Modified: July 27,2017
+Last Modified: March 13,2018
 
 Licensed under the GNU General Public License Version 3 (GNU GPL v3),
     available at: https://www.gnu.org/licenses/gpl-3.0.txt
 
-(C) 2017 K4YT3X
+(C) 2017 - 2018 K4YT3X
 """
 
+import argparse
 import configparser
 import os
 import psutil
@@ -68,8 +69,13 @@ except ImportError:
             print('\033[31m\033[1mInvalid Input!\033[0m')
 
 CONFPATH = '/etc/shadowagent.conf'
-PIDF = '/var/run/shadowsocks.pid'
-VERSION = '1.1'
+if os.getuid() == 0:
+    PIDF = '/var/run/shadowsocks.pid'
+    LOGF = '/var/log/shadowsocks.log'
+else:
+    PIDF = '/tmp/shadowsocks.pid'
+    LOGF = '/tmp/shadowsocks.log'
+VERSION = '1.2'
 
 
 # -------------------------------- Classses --------------------------------
@@ -79,7 +85,8 @@ class silkroad:
     def start():
         avalon.info('Starting silkroad VPN')
         if not silkroad.is_running():
-            os.system('/usr/bin/sslocal -s ' + server_addr + ' -p ' + server_port + ' -k ' + server_pswd + ' -b 127.0.0.1 -l ' + local_port + ' -m ' + encryption_method + ' -d start')
+            os.system('/usr/bin/env sslocal -s {} -p {} -k {} -b 127.0.0.1 -l {} -m {} --pid-file={} --log-file={} -d start'.format(
+                server_addr, server_port, server_pswd, local_port, encryption_method, PIDF, LOGF))
         else:
             avalon.error(avalon.FM.RBD + 'Silkroad VPN is ' + avalon.FM.BD + 'already running')
 
@@ -106,6 +113,25 @@ class silkroad:
 
 
 # -------------------------------- Functions --------------------------------
+
+
+def process_args():
+    parser = argparse.ArgumentParser()
+    control_group = parser.add_argument_group('Controls')
+    control_group.add_argument("--start", help="Start connection to a server", action="store_true", default=False)
+    control_group.add_argument("--stop", help="Stop currently running sessions", action="store_true", default=False)
+    control_group.add_argument("--status", help="Check ShadowAgent status", action="store_true", default=False)
+    control_group.add_argument("-s", help="Start a server by index", action="store", default=False)
+    inst_group = parser.add_argument_group('Installation')
+    inst_group.add_argument("--install", help="Install ShadowAgent to system", action="store_true", default=False)
+    inst_group.add_argument("--uninstall", help="Uninstall ShadowAgent from system", action="store_true", default=False)
+    inst_group.add_argument("--reconfigure", help="Reconfigure servers", action="store_true", default=False)
+    etc = parser.add_argument_group('Extra')
+    etc.add_argument("--version", help="Show SCUTUM version and exit", action="store_true", default=False)
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
+    return parser.parse_args()
 
 
 def validIP(s):
@@ -221,7 +247,7 @@ def setup_wizard():
     exit(0)
 
 
-def select_server():
+def select_server(serverid=False):
     """
         List all servers and let the use choose
     """
@@ -234,13 +260,16 @@ def select_server():
         print(avalon.FG.Y + str(id) + ': ' + avalon.FM.RST + server)
         id += 1
     print('')
-    while True:
-        serverid = avalon.gets('Select Server #: ')
-        try:
-            return servers_numerical[int(serverid)]
-            break
-        except IndexError:
-            avalon.error('Selected Server not found!')
+    if serverid is False:
+        while True:
+            serverid = avalon.gets('Select Server #: ')
+            try:
+                return servers_numerical[int(serverid)]
+                break
+            except IndexError:
+                avalon.error('Selected Server not found!')
+    else:
+        return servers_numerical[int(serverid)]
 
 
 def parse_config():
@@ -265,71 +294,71 @@ def parse_config():
 
 # -------------------------------- Procedural --------------------------------
 
+servers = parse_config()
+args = process_args()
+
 os.system('clear')
 print('\n\n')
 print_logo()
 
-if os.getuid() != 0:
-    avalon.error('This application must be run as root!')
+if args.version:  # prints program legal / dev / version info
+    print("Current Version: " + VERSION)
+    print("Author: K4YT3X")
+    print("License: GNU GPL v3")
+    print("Github Page: https://github.com/K4YT3X/shadowagent")
+    print("Contact: k4yt3x@protonmail.com")
+    print()
     exit(0)
 
-servers = parse_config()
 
-try:
-    if sys.argv[1].upper() == 'START':
-        server_name = select_server()
-        config = configparser.ConfigParser()
-        config.read(CONFPATH)
-        config.sections()
-        servers = config['SERVERS']
-        ports = config['PORTS']
-        localports = config['LOCALPORTS']
-        passwords = config['PASSWORDS']
-        encryptions = config['ENCRYPTIONS']
-        server_port = ports[server_name]
-        local_port = localports[server_name]
-        server_addr = servers[server_name]
-        server_pswd = passwords[server_name]
-        encryption_method = encryptions[server_name]
-        silkroad.start()
-        avalon.info('OK')
-    elif sys.argv[1].upper() == 'STOP':
-        silkroad.stop()
-        avalon.info('OK')
-    elif sys.argv[1].upper() == 'STATUS':
-        if silkroad.is_running():
-            print(avalon.FM.BD + '[+] Silkroad: ' + avalon.FG.G + 'RUNNING' + avalon.FM.RST)
-        else:
-            print(avalon.FM.BD + '[+] Silkroad: ' + avalon.FG.R + 'STOPPED' + avalon.FM.RST)
-        exit(0)
-    elif sys.argv[1].upper() == 'INSTALL':
-        avalon.info('Installing ShadowAgent')
-        os.system('cp ' + os.path.abspath(__file__) + ' /usr/bin/sa')  # os.rename throws an error when /tmp is in a separate partition
-        os.system('chown root: /usr/bin/sa')
-        os.system('chmod 755 /usr/bin/sa')
-        avalon.info('Installation Complete!')
-        avalon.info('Now you can use "sa" command to start ShadowAgent')
-        if os.path.isfile(CONFPATH):
-            if avalon.ask('Config file already exists. Overwrite?', False):
-                setup_wizard()
-            else:
-                exit(0)
-        else:
-            setup_wizard()
-    elif sys.argv[1].upper() == 'RECONFIGURE':
-        avalon.warning('Reconfiguring will purge all settings!')
-        if avalon.ask('Are you sure that you want to reconfigure silkroad VPN client?', False):
-            print('\nAlright then ~\n')
+if args.start or args.s:
+    server_name = select_server(args.s)
+    config = configparser.ConfigParser()
+    config.read(CONFPATH)
+    config.sections()
+    servers = config['SERVERS']
+    ports = config['PORTS']
+    localports = config['LOCALPORTS']
+    passwords = config['PASSWORDS']
+    encryptions = config['ENCRYPTIONS']
+    server_port = ports[server_name]
+    local_port = localports[server_name]
+    server_addr = servers[server_name]
+    server_pswd = passwords[server_name]
+    encryption_method = encryptions[server_name]
+    if int(local_port) < 1025 and os.getuid() != 0:
+        avalon.error('This application must be run as root to bind on port {}'.format(local_port))
+        exit(1)
+    silkroad.start()
+    avalon.info('OK')
+elif args.stop:
+    silkroad.stop()
+    avalon.info('OK')
+elif args.status:
+    if silkroad.is_running():
+        print(avalon.FM.BD + '[+] Silkroad: ' + avalon.FG.G + 'RUNNING' + avalon.FM.RST)
+    else:
+        print(avalon.FM.BD + '[+] Silkroad: ' + avalon.FG.R + 'STOPPED' + avalon.FM.RST)
+    exit(0)
+elif args.install:
+    avalon.info('Installing ShadowAgent')
+    os.system('cp ' + os.path.abspath(__file__) + ' /usr/bin/sa')  # os.rename throws an error when /tmp is in a separate partition
+    os.system('chown root: /usr/bin/sa')
+    os.system('chmod 755 /usr/bin/sa')
+    avalon.info('Installation Complete!')
+    avalon.info('Now you can use "sa" command to start ShadowAgent')
+    if os.path.isfile(CONFPATH):
+        if avalon.ask('Config file already exists. Overwrite?', False):
             setup_wizard()
         else:
-            avalon.warning('Aborting reconfiguration')
             exit(0)
     else:
-        avalon.error('Unrecognized Option: ' + sys.argv[1] + '\n')
-        print_help()
-except IndexError:
-    print_help()
-    exit(0)
-except Exception as e:
-    avalon.error(str(e))
-    exit(1)
+        setup_wizard()
+elif args.reconfigure:
+    avalon.warning('Reconfiguring will purge all settings!')
+    if avalon.ask('Are you sure that you want to reconfigure silkroad VPN client?', False):
+        print('\nAlright then ~\n')
+        setup_wizard()
+    else:
+        avalon.warning('Aborting reconfiguration')
+        exit(0)
